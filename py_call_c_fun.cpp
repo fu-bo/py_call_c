afd9bd87d79c4c8939a1e963b4d0a7f2e8e9bbb5
Bo Fu <bofu@umich.edu>
2021-08-23 16:34:55 -0400

    Address the situation of multiple outputs.

--- py_call_c_fun.cpp
+++ py_call_c_fun.cpp
@@ -14,7 +14,7 @@ private:
 public:
     PyCallCFun(/* args */);
     ~PyCallCFun();
-    PyObject* addMulMatrix(PyObject *matrixObj, PyObject *factor);
+    PyObject* addMulMatrix(PyObject *matrixObj, int factor);
 };
 
 PyCallCFun::PyCallCFun(/* args */) {
@@ -23,7 +23,8 @@ PyCallCFun::PyCallCFun(/* args */) {
 PyCallCFun::~PyCallCFun() {
 }
 
-PyObject* PyCallCFun::addMulMatrix(PyObject *matrixObj, PyObject *factor) {
+PyObject* PyCallCFun::addMulMatrix(PyObject *matrixObj, int factor) {
+    std::cout << "c++: factor = " << factor << "\n";
     PyArrayObject * matrixArray = reinterpret_cast<PyArrayObject *>(matrixObj);
     int matrixNDim = PyArray_NDIM(matrixArray);
     if (matrixNDim != 2) {
@@ -34,8 +35,8 @@ PyObject* PyCallCFun::addMulMatrix(PyObject *matrixObj, PyObject *factor) {
     npy_intp matrixRows = matrixSize[0], matrixCols = matrixSize[1];
     void* matrixPtr = PyArray_DATA(matrixArray);
     int* intmatrixPtr = static_cast<int *>(matrixPtr);
-    std::cout << "matrixRows = " << matrixRows << "\n";
-    std::cout << "matrixCols = " << matrixCols << "\n";
+    std::cout << "c++: matrixRows = " << matrixRows << "\n";
+    std::cout << "c++: matrixCols = " << matrixCols << "\n";
     for (int iRow = 0; iRow < matrixRows; iRow++) {
         for (int iCol = 0; iCol < matrixCols; iCol++) {
             int temp = intmatrixPtr[iRow * matrixCols + iCol];
@@ -43,9 +44,40 @@ PyObject* PyCallCFun::addMulMatrix(PyObject *matrixObj, PyObject *factor) {
         }
         std::cout << "\n";
     }
-    return matrixObj;
-}
 
+    // Py_Initialize();
+    import_array();
+
+    npy_intp inputSize[2];
+    inputSize[0] = 2;
+    inputSize[1] = 3;
+    double inputData[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    PyObject* input = PyArray_SimpleNewFromData(2, inputSize, PyArray_FLOAT64, inputData);
+
+    // Construct outputs
+    int outputNumel = matrixRows * matrixCols;
+    int* addMat = new int[outputNumel]; // int32_t
+    int* mulMat = new int[outputNumel]; // int32_t
+    for (int i = 0; i < outputNumel; i++) {
+        addMat[i] = intmatrixPtr[i] + factor;
+        mulMat[i] = intmatrixPtr[i] * factor;
+    }
+    std::cout << "outputNumel = " << outputNumel <<"\n";
+    std::cout << "matrixSize[0] = " << matrixSize[0] <<"\n";
+    std::cout << "matrixSize[1] = " << matrixSize[1] <<"\n";
+
+    PyObject* addMatArray = PyArray_SimpleNewFromData(2, matrixSize, PyArray_INT32, addMat);
+    PyObject* mulMatArray = PyArray_SimpleNewFromData(2, matrixSize, PyArray_INT32, mulMat);
+    PyObject* outputTuple = PyTuple_New(2);
+    int flagSuccess = 0;
+    flagSuccess = PyTuple_SetItem(outputTuple, 0, addMatArray);
+    flagSuccess = PyTuple_SetItem(outputTuple, 1, mulMatArray);
+
+    // Should not do the follwing
+    // delete[] addMat;
+    // delete[] mulMat;
+    return outputTuple;
+}
 
 using namespace boost::python;
 BOOST_PYTHON_MODULE(py_call_c_fun)
--- py_call_c_main.py
+++ py_call_c_main.py
@@ -4,7 +4,7 @@ sys.path.insert(1, './build/')
 from py_call_c_fun import py_call_c_fun
 
 mat = np.arange(6).reshape(2,3).astype(np.int32)
-factor = 2
+factor = 5
 temp_var = py_call_c_fun()
 new_mat = temp_var.add_mul_matrix(mat, factor)
 print('new_mat = ', new_mat)
